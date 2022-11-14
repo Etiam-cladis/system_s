d805e538abacda70512bc45a95491a858d7e019b
lala <lnmemq@gmail.com>
2022-11-14 20:32:39 +0800

    update zsh

---
+++ zsh_s/.zplug/cache/theme.zsh
@@ -0,0 +1,3 @@
+__zplug::core::load::as_theme --repo "themes/crunch" "/home/mengnan/.zplug/repos/robbyrussell/oh-my-zsh/lib/git.zsh"
+__zplug::core::load::as_theme --repo "themes/crunch" "/home/mengnan/.zplug/repos/robbyrussell/oh-my-zsh/lib/prompt_info_functions.zsh"
+__zplug::core::load::as_theme --repo "themes/crunch" "/home/mengnan/.zplug/repos/robbyrussell/oh-my-zsh/themes/crunch.zsh-theme"
